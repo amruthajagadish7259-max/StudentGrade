87777b12021a2b3dc6fbc50c4fb054c579abac0a
amruthajagadish7259-max <amruthajagadish7259@gmail.com>
2025-11-11 09:14:42 +0530

    Update studgrade.py

--- studgrade.py
+++ studgrade.py
@@ -1,19 +1,22 @@
 import sys
-if len(sys.argv)==5:
-     marks_1=sys.argv[0]
-     marks_2=sys.argv[1]
-     marks_3=sys.argv[2]
-     marks_4=sys.argv[3]
-     marks_5=sys.argv[4]
+if len(sys.argv)==6:
+     name=sys.argv[0]
+     marks_1=sys.argv[1]
+     marks_2=sys.argv[2]
+     marks_3=sys.argv[3]
+     marks_4=sys.argv[4]
+     marks_5=sys.argv[5]
 else:
+     name=xyz
      marks_1=50
      marks_2=70
      marks_3=85
      marks_4=90
      marks_5=85
 
-print("English", marks_1)
-print("Maths", marks_2)
+print(f"file:{name}")
+print(f"Eng:{marks_1}")
+print(f"math:{marks_2}")
 print(f"Devops:{marks_3}")
 print(f"SSW:{marks_4}")
 print(f"SE:{marks_5}")
